1f98dced33dfbc831aac2be3e51f63a608a6bcf4
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-08-10 16:00:16 +0300

    update

--- app/_component/AboutSection/AboutSection.jsx
+++ app/_component/AboutSection/AboutSection.jsx
@@ -3,10 +3,13 @@ import image from "../../img/Light_Logo.svg";
 export default function AboutSection() {
   return (
     <>
-      <div className="image-darta" style={{ textAlign: "right" }}>
+      <div
+        className="image-darta"
+        style={{ textAlign: "right", zIndex: "100" }}
+      >
         <Image width={400} height={100} src={image} alt="" />
       </div>
-      <div className="container" style={{margin:'100px auto 100px auto'}}>
+      <div className="container" style={{ margin: "100px auto 100px auto" }}>
         <div className="all-about grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-16">
           <div className="box">
             <h1>Lorem</h1>
@@ -18,7 +21,9 @@ export default function AboutSection() {
             </p>
           </div>
           <div className="box">
-            <span className="eight">8</span>
+            <div className="maskAbs">
+              <span className="eight">8</span>
+            </div>
             <h1>
               <span>Ye</span>
               <span>ar</span>
