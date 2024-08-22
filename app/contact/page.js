9b18a3a1d42d637840df99a8ae3edc06cb0c697f
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-08-22 21:59:49 +0300

    update

--- app/contact/page.js
+++ app/contact/page.js
@@ -60,7 +60,7 @@ const Contact = () => {
       <title>Contact - Wesmart For Technology</title>
       <div className="contactP">
         <Landing className="olurmu">
-          <div style={{ top: "110px", position: "relative" }}>
+          <div style={{ top: "110px", position: "relative" }} className="raf3">
             <section>
               <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
                 <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
