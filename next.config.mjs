4c68810e25f4b751814e71c30630e3dc81f40289
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-09-03 11:53:22 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@ import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    loader: "custom",
-    loaderFile: "./my-loader.js",
-  },
+//   images: {
+//     loader: "custom",
+//     loaderFile: "./my-loader.js",
+//   },
 };
 
 export default withNextVideo(nextConfig, { folder: "movable" });
